18855666c5220b22ad6556a5402914ac4fea1c04
Christophe Kalista <christophe.kalista@gmail.com>
2018-01-03 14:37:27 +0000

    PP-3032 Applied code review comment

--- app/controllers/adhoc_payment/how_to_pay_controller.js
+++ app/controllers/adhoc_payment/how_to_pay_controller.js
@@ -1,9 +1,7 @@
 'use strict'
 
-const HOW_TO_PAY_VIEW = 'adhoc_payment/how-to-pay'
-
 module.exports = (req, res) => {
   res
     .status(200)
-    .render(HOW_TO_PAY_VIEW)
+    .render('adhoc_payment/how-to-pay')
 }
