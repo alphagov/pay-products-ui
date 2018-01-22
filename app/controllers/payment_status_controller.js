6daa9acd358cda4340c6019f12b706eea7686988
Kasun Kumara <kasun.kumara@gmail.com>
2018-01-22 16:26:03 +0000

    PP-3034 Adding payment status controller
    - making it generic as payment status is not limited only to ADHOC payments
    - refactored out confirmation/failed views in to a generic `pay` directory

--- app/controllers/payment_status_controller.js
+++ app/controllers/payment_status_controller.js
@@ -1,22 +1,35 @@
 'use strict'
 
+// Node.js core dependencies
+const currencyFormatter = require('currency-formatter')
+
 // Custom dependencies
 const response = require('../utils/response').response
+const {pay} = require('../paths')
 
-function beautify (reference, spacingPosition) {
-  let chunks = []
-  let i
-  for (i = -1; i < reference.length; i += spacingPosition) {
-    chunks.push(reference.substr(i, spacingPosition))
-  }
-  return chunks
+function asGBP (amountInPence) {
+  return currencyFormatter.format((amountInPence / 100).toFixed(2), {code: 'GBP'})
+}
+
+function beautify (reference) {
+  return reference.slice(0, 3) + ' ' + reference.slice(3, 7) + ' ' + reference.slice(7)
 }
 
 module.exports = (req, res) => {
+  const product = req.product
   const payment = req.payment
   const data = {
-    referenceNumber: beautify(payment.referenceNumber, 4).join('-'),
-    status: payment.govukStatus
+    currentServiceName: product.serviceName
   }
-  response(req, res, 'adhoc-payment/confirmation', data)
+
+  if (payment.govukStatus.toLowerCase() === 'success') {
+    data.payment = {
+      reference: beautify(payment.referenceNumber),
+      amount: asGBP(payment.amount)
+    }
+    return response(req, res, 'pay/confirmation', data)
+  } else {
+    data.backToStartPage = pay.product.replace(':productExternalId', product.externalId)
+    return response(req, res, 'pay/failed', data)
+  }
 }
--- test/unit/controllers/payment_complete_controller_test.js
+++ test/unit/controllers/payment_complete_controller_test.js
@@ -7,10 +7,10 @@ const nock = require('nock')
 const supertest = require('supertest')
 
 // Local dependencies
-const {PRODUCTS_URL} = require('../../../../config')
-const {getApp} = require('../../../../server')
-const productFixtures = require('../../../fixtures/product_fixtures')
-const paths = require('../../../../app/paths')
+const {PRODUCTS_URL} = require('../../../config/index')
+const {getApp} = require('../../../server')
+const productFixtures = require('../../fixtures/product_fixtures')
+const paths = require('../../../app/paths')
 
 describe('payment complete controller', () => {
   describe('when a demo payment is returned', () => {
@@ -70,6 +70,7 @@ describe('payment complete controller', () => {
       })
     })
   })
+
   describe('when a PROTOTYPE payment is returned', () => {
     let product, payment, response
     before(done => {
@@ -99,6 +100,77 @@ describe('payment complete controller', () => {
       expect(response.headers).to.have.property('location').to.equal(product.return_url)
     })
   })
+
+  describe('when a ADHOC payment is returned', () => {
+    let product, payment, response, $
+    describe('when the payment was a success', () => {
+      before(done => {
+        product = productFixtures.validCreateProductResponse({
+          type: 'ADHOC'
+        }).getPlain()
+        payment = productFixtures.validCreatePaymentResponse({
+          product_external_id: product.external_id,
+          amount: 2000,
+          reference_number: 'ABCD1234EF',
+          govuk_status: 'success'
+        }).getPlain()
+        nock(PRODUCTS_URL).get(`/v1/api/products/${product.external_id}`).reply(200, product)
+        nock(PRODUCTS_URL).get(`/v1/api/payments/${payment.external_id}`).reply(200, payment)
+
+        supertest(getApp())
+          .get(paths.pay.complete.replace(':paymentExternalId', payment.external_id))
+          .end((err, res) => {
+            response = res
+            $ = cheerio.load(res.text || '')
+            done(err)
+          })
+      })
+
+      it('should respond with status code 200', () => {
+        expect(response.statusCode).to.equal(200)
+      })
+
+      it('should redirect to the payment success page', () => {
+        expect($('title').text()).to.include(`Your payment was successful - ${product.service_name}`)
+        expect($('#payment-reference').text()).to.include(`ABC D123 4EF`)
+        expect($('#payment-amount').text()).to.include(`£20.00`)
+      })
+    })
+
+    describe('when the payment has failed', () => {
+      before(done => {
+        product = productFixtures.validCreateProductResponse({
+          type: 'ADHOC'
+        }).getPlain()
+        payment = productFixtures.validCreatePaymentResponse({
+          product_external_id: product.external_id,
+          amount: 2000,
+          reference_number: 'ABCD1234EF',
+          govuk_status: 'failure'
+        }).getPlain()
+        nock(PRODUCTS_URL).get(`/v1/api/products/${product.external_id}`).reply(200, product)
+        nock(PRODUCTS_URL).get(`/v1/api/payments/${payment.external_id}`).reply(200, payment)
+
+        supertest(getApp())
+          .get(paths.pay.complete.replace(':paymentExternalId', payment.external_id))
+          .end((err, res) => {
+            response = res
+            $ = cheerio.load(res.text || '')
+            done(err)
+          })
+      })
+
+      it('should respond with status code 200', () => {
+        expect(response.statusCode).to.equal(200)
+      })
+
+      it('should redirect to the payment success page', () => {
+        expect($('title').text()).to.include(`Your payment has been declined - ${product.service_name}`)
+        expect($('#restart-payment').children('a').attr('href')).to.equal(`/pay/${product.external_id}`)
+      })
+    })
+  })
+
   describe('when a payment lookup fails', () => {
     let payment, response, $
     before(done => {
