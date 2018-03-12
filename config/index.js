7b8a164ed98dc275bc22de8cd81e3153ca853775
Belinda Cowey <belinda.cowey@digital.cabinet-office.gov.uk>
2018-03-12 10:23:44 +0000

    PP-3379 Change documentation link in header and footer
    Update all referenced links to new url
    Added npm-shrinkwrap.json to build docker file as sntp has been upgraded
    and we need to pin it our version
    
    solo @belindac

--- config/index.js
+++ config/index.js
@@ -3,4 +3,4 @@
 exports.CORRELATION_HEADER = process.env.CORRELATION_HEADER_NAME || 'x-request-id'
 exports.SELFSERVICE_TRANSACTIONS_URL = process.env.SELFSERVICE_TRANSACTIONS_URL
 exports.PRODUCTS_URL = process.env.PRODUCTS_URL
-exports.DOCS_URL = 'https://govukpay-docs.cloudapps.digital'
+exports.DOCS_URL = 'https://docs.payments.service.gov.uk'
