085544097a59b76d8c69b1c60c4566f9ef5d0ada
Stephen Daly <stephencdaly@gmail.com>
2019-04-11 13:31:30 +0100

    PP-4986 Internationalise reference server validation
    - Internationalise server-side errors for the reference page.
    - Add server-side check for disallowed characters.
    - Use the res.locals.__p in controllers rather than initialising i18n each time.

--- test/unit/controllers/payment_reference/post_product_reference_controller_test.js
+++ test/unit/controllers/payment_reference/post_product_reference_controller_test.js
@@ -90,7 +90,7 @@ describe('product reference post controller', function () {
       expect($('h1').text()).to.include(product.name)
       expect($('p#description').text()).to.include(product.description)
       expect($('form').attr('action')).to.equal(`/pay/reference/${product.external_id}`)
-      expect($('.govuk-heading-m').text()).to.include(`Enter a Test reference label`)
+      expect($('.govuk-heading-m').text()).to.include(`Enter a valid Test reference label`)
     })
   })
 
@@ -128,7 +128,45 @@ describe('product reference post controller', function () {
       expect($('h1').text()).to.include(product.name)
       expect($('p#description').text()).to.include(product.description)
       expect($('form').attr('action')).to.equal(`/pay/reference/${product.external_id}`)
-      expect($('.govuk-heading-m').text()).to.include(`The Test reference label is not valid`)
+      expect($('.govuk-heading-m').text()).to.include('Text is too long')
+    })
+  })
+
+  describe('when reference field contains disallowed characters', function () {
+    before(done => {
+      product = productFixtures.validCreateProductResponse({
+        type: 'ADHOC',
+        reference_enabled: true,
+        reference_label: 'Test reference label'
+      }).getPlain()
+      service = serviceFixtures.validServiceResponse().getPlain()
+      nock(config.PRODUCTS_URL).get(`/v1/api/products/${product.external_id}`).reply(200, product)
+      nock(config.ADMINUSERS_URL).get(`/v1/api/services?gatewayAccountId=${product.gateway_account_id}`).reply(200, service)
+
+      const referenceNumber = '<>'
+      supertest(createAppWithSession(getApp()))
+        .post(paths.pay.reference.replace(':productExternalId', product.external_id))
+        .send({
+          'payment-reference': referenceNumber,
+          csrfToken: csrf().create('123')
+        })
+        .end((err, res) => {
+          response = res
+          $ = cheerio.load(res.text || '')
+          done(err)
+        })
+    })
+
+    it('should respond with code: 200 OK', () => {
+      expect(response.statusCode).to.equal(200)
+    })
+
+    it('should render product reference start page with error message', () => {
+      expect($('title').text()).to.include(service.service_name.en)
+      expect($('h1').text()).to.include(product.name)
+      expect($('p#description').text()).to.include(product.description)
+      expect($('form').attr('action')).to.equal(`/pay/reference/${product.external_id}`)
+      expect($('.govuk-heading-m').text()).to.include(`You can’t use any of the following characters`)
     })
   })
 })
