5a6665b38a1f37bddfa735eba89c6f0901876e40
Steven <steven.fountain@digital.cabinet-office.gov.uk>
2021-09-28 12:57:52 +0100

    PP-8688: add support for captcha enterprise version
    The enterprise version of captcha expects a different interaction:
    * POST body JSON instead of xhtml/form data
    * secret key passed in through `?key=` paraemeter to endpoint
    * returns a likelihood score rather than a simple pass/ fail response
    * assesment passed as an `event` instead of a one off token
    
    Fork the existing method to make these changes and decide whether to use
    basic or enteprise with an environment flag.

--- app/utils/captcha.js
+++ app/utils/captcha.js
@@ -1,13 +1,67 @@
 const request = require('request')
 const logger = require('./logger')(__filename)
 
-const { GOOGLE_RECAPTCHA_SECRET_KEY, GOOGLE_RECAPTCHA_VERIFY_URL } = process.env
+const {
+  GOOGLE_RECAPTCHA_SECRET_KEY,
+  GOOGLE_RECAPTCHA_VERIFY_URL,
+  GOOGLE_RECAPTCHA_SITE_KEY,
+  GOOGLE_RECAPTCHA_USE_ENTERPRISE_VERSION
+} = process.env
 const HTTP_SUCCESS_CODE = 200
 
-function verifyCAPTCHAToken(token) {
+function verifyCAPTCHAEnterpriseVersion(token) {
   return new Promise((resolve, reject) => {
     if (!GOOGLE_RECAPTCHA_SECRET_KEY) {
       reject(new Error('reCAPTCHA secret key not set in environment'))
+      return
+    }
+    if(!token) {
+      reject(new Error('no reCAPTCHA enterprise widget token response provided'))
+      return
+    }
+    request.post({
+      url: GOOGLE_RECAPTCHA_VERIFY_URL,
+      proxy: process.env.http_proxy,
+      qs: {
+        key: GOOGLE_RECAPTCHA_SECRET_KEY
+      },
+      body: {
+        event: {
+          token: token,
+          siteKey: GOOGLE_RECAPTCHA_SITE_KEY
+        }
+      },
+      json: true
+    }, (error, response, body) => {
+      if (error) {
+        reject(error)
+        return
+      }
+      if (response.statusCode === HTTP_SUCCESS_CODE) {
+        // https://cloud.google.com/recaptcha-enterprise/docs/interpret-assessment
+        if (!body.score || body.score < 0.9) {
+          logger.info('Failed reCAPTCHA response', {
+            tokenProperties: body.tokenProperties,
+            score: body.score,
+            reasons: body.reasons
+          })
+        }
+        resolve(true)
+        return
+      }
+      reject(new Error(`Unknown reCAPTCHA response ${response.statusCode}`))
+    })
+  })
+}
+
+function verifyCAPTCHATokenBasicVersion(token) {
+  return new Promise((resolve, reject) => {
+    if (!GOOGLE_RECAPTCHA_SECRET_KEY) {
+      reject(new Error('reCAPTCHA secret key not set in environment'))
+      return
+    }
+    if(!token) {
+      reject(new Error('no reCAPTCHA widget token response provided'))
       return
     }
     request.post({
@@ -37,4 +91,4 @@ function verifyCAPTCHAToken(token) {
   })
 }
 
-module.exports = { verifyCAPTCHAToken }
+module.exports = { verifyCAPTCHAToken: GOOGLE_RECAPTCHA_USE_ENTERPRISE_VERSION === 'true' ? verifyCAPTCHAEnterpriseVersion : verifyCAPTCHATokenBasicVersion }
