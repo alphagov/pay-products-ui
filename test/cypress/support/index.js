4caf44b6a472be68707a5fe9f514606e12e2b8b7
Iqbal Ahmed <iqbal.ahmed@digital.cabinet-office.gov.uk>
2022-02-15 16:13:09 +0000

    PP-8970 Cypress - initial setup
    - Add config files for setting up Cypress.
    - Most of this was taken from Self service and modified.
    - Updated the package.json:
      - Added the new Cypress dependencies:
        - Cypress
        - Mountebank
        - request-promise-native
      - Add the Cypress run scripts.

---
+++ test/cypress/support/index.js
@@ -0,0 +1,18 @@
+// ***********************************************************
+// This file is processed and loaded automatically before
+// Cypress test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+beforeEach(() => {
+  cy.task('clearStubs')
+})
+
+afterEach(() => {
+  cy.task('verifyStubs')
+})
