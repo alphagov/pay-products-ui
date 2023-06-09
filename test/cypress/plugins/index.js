f0f7a4334882eb6fe93a5d3155e8e5d4b1996c3e
Krishna Bottla <40598480+kbottla@users.noreply.github.com>
2023-06-09 13:00:48 +0100

    PP-10875 Remove request library usage from cypress tests
    - Replaced request library usage in tests with Axios. This is to eventually remove the request library dependency which is not maintained anymore

--- test/cypress/plugins/index.js
+++ test/cypress/plugins/index.js
@@ -7,7 +7,7 @@
 
 'use strict'
 
-const request = require('request-promise-native')
+const axios = require('axios')
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
@@ -23,48 +23,45 @@ module.exports = (on, config) => {
      * the same call.
      */
     setupStubs (stubs) {
-      return request({
-        method: 'POST',
-        url: mountebankImpostersUrl,
-        json: true,
-        body: {
+      return axios.post(mountebankImpostersUrl,
+        {
           port: config.env.MOUNTEBANK_IMPOSTERS_PORT,
           protocol: 'http',
           stubs
-        }
-      })
+        })
+        .then(function () { return ''})
+        .catch(function (error) { throw error})
     },
     /**
      * Makes a request to Mountebank to delete the existing Imposter along with all stubs that have been set up.
      */
     clearStubs () {
-      return request.delete(mountebankImpostersUrl)
+      return axios.delete(mountebankImpostersUrl)
+        .then(function () { return ''})
+        .catch(function (error) { throw error})
     },
     /**
      * Makes a request to Mountebank to verify that stubs have been called the expected number of times
      */
     verifyStubs () {
-      return request({
-        method: 'GET',
-        url: `${mountebankImpostersUrl}/${config.env.MOUNTEBANK_IMPOSTERS_PORT}`,
-        json: true
-      }).then(response => {
-        response.stubs.forEach((stub) => {
-          // NOTE: if the "verifyCalledTimes" is specified for a stub, we will attempt to verify
-          // for all `it` blocks the stub is setup for, and the counter is reset for every `it`.
-          if (stub.verifyCalledTimes) {
-            // the matches array is added to stubs only when Mountebank is run with the --debug flag
-            const timesCalled = (stub.matches && stub.matches.length) || 0
-            if (timesCalled !== stub.verifyCalledTimes) {
-              throw new Error(`Expected stub '${stub.name}' to be called ${stub.verifyCalledTimes} times, but was called ${timesCalled} times`)
+      return axios.get(`${mountebankImpostersUrl}/${config.env.MOUNTEBANK_IMPOSTERS_PORT}`)
+        .then(function (response){
+          response.data.stubs.forEach((stub) => {
+            // NOTE: if the "verifyCalledTimes" is specified for a stub, we will attempt to verify
+            // for all `it` blocks the stub is setup for, and the counter is reset for every `it`.
+            if (stub.verifyCalledTimes) {
+              // the matches array is added to stubs only when Mountebank is run with the --debug flag
+              const timesCalled = (stub.matches && stub.matches.length) || 0
+              if (timesCalled !== stub.verifyCalledTimes) {
+                throw new Error(`Expected stub '${stub.name}' to be called ${stub.verifyCalledTimes} times, but was called ${timesCalled} times`)
+              }
             }
-          }
-        })
+          })
 
-        return null
-      })
-        .catch(err => {
-          if (err.statusCode === 404) {
+          return ''
+        })
+        .catch(function (err){
+          if (err.status === 404) {
             // imposter probably hasn't been added in Mountebank as no stubs were setup for the current
             // test
             return null
