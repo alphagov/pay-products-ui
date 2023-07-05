facb5de6379bc968cd0631c8024a5fafde4b1c4a
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2023-07-05 16:10:40 +0100

    BAU: Fix breaking method change in envfile
    See Changelog: https://github.com/bevry/envfile/blob/master/HISTORY.md#v600-2020-may-21

--- test/test-helpers/test-env.js
+++ test/test-helpers/test-env.js
@@ -2,7 +2,7 @@
 const path = require('path')
 const envfile = require('envfile')
 
-const TEST_ENV = envfile.parseFileSync(path.join(__dirname, '../test.env'))
+const TEST_ENV = envfile.parse(path.join(__dirname, '../test.env'))
 
 for (const property in TEST_ENV) {
   process.env[property] = TEST_ENV[property]
