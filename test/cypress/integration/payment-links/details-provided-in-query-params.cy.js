639380afc5cf3b8e9d9996aa334e58e3bdb42ba5
John Shields <john.shields@digital.cabinet-office.gov.uk>
2024-01-17 11:11:05 +0000

    PP-11978 - Improve error message on pre-filled payment links
    - Amend page title and page heading
    - Reference new payment link template
    - Update associated unit tests to reflect changes
    - Update Cypress integration tests to reflect changes

--- test/cypress/integration/payment-links/details-provided-in-query-params.cy.js
+++ test/cypress/integration/payment-links/details-provided-in-query-params.cy.js
@@ -70,8 +70,7 @@ describe('Payment link visited with invalid amount in query params', () => {
 
   it('should show an error page when the payment link is visited', () => {
     cy.visit(`/redirect/${serviceNamePath}/${productNamePath}?amount=not-valid`, { failOnStatusCode: false })
-    cy.get('h1').should('have.text', 'An error occurred:')
-    cy.get('[data-cy=error-message]').should('contain.text', 'Enter an amount in pence. For example ’2000’.')
+    cy.get('h1').should('have.text', 'There is a problem')
     cy.get('[data-cy=error-message]').should('contain.text', 'There is a problem with the link you have been sent to use to pay. Please contact the service you are trying to make a payment to.')
   })
 })
@@ -89,7 +88,7 @@ describe('Payment link visited with invalid reference in query params', () => {
 
   it('should show an error page when the payment link is visited', () => {
     cy.visit(`/redirect/${serviceNamePath}/${productNamePath}?reference=<>`, { failOnStatusCode: false })
-    cy.get('h1').should('have.text', 'An error occurred:')
+    cy.get('h1').should('have.text', 'There is a problem')
     cy.get('[data-cy=error-message]').should('contain.text', 'Reference must be 255 characters or fewer. You cannot use any of the following characters < > ; : ` ( ) " = | "," ~ [ ]')
     cy.get('[data-cy=error-message]').should('contain.text', 'There is a problem with the link you have been sent to use to pay. Please contact the service you are trying to make a payment to.')
   })
