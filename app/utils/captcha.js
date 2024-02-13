534aeafc8a624bb051fc5ad347815b6c2e813f7e
Iqbal AHmed <iqbal.ahmed@digital.cabinet-office.gov.uk>
2024-02-13 14:18:41 +0000

    PP-11683 Replace `request` with `axios` for captcha
    - Update `captcha` code to use Axios.
    - Remove the `request` NPM module.
    - Update the tests
    - Add extra test coverage for the enterprise Captcha path.
       - As we did not have any tests to cover enterprise path.
       - Had to update the Captcha code to make it easier to test.

--- app/utils/captcha.js
+++ app/utils/captcha.js
@@ -1,11 +1,10 @@
-const request = require('request')
+const axios = require('axios')
 const logger = require('./logger')(__filename)
 const urlJoin = require('url-join')
 
 const {
   GOOGLE_RECAPTCHA_SECRET_KEY,
   GOOGLE_RECAPTCHA_SITE_KEY,
-  GOOGLE_RECAPTCHA_USE_ENTERPRISE_VERSION,
   GOOGLE_RECAPTCHA_ENTERPRISE_PROJECT_ID
 } = process.env
 const HTTP_SUCCESS_CODE = 200
@@ -16,92 +15,88 @@ function formatEnterpriseUrl (projectId) {
   return urlJoin('https://recaptchaenterprise.googleapis.com/v1beta1/projects', String(projectId), 'assessments')
 }
 
-function verifyCAPTCHAEnterpriseVersion (token) {
-  return new Promise((resolve, reject) => {
-    if (!GOOGLE_RECAPTCHA_SECRET_KEY) {
-      reject(new Error('reCAPTCHA secret key not set in environment'))
-      return
-    }
-    if (!token) {
-      logger.warn('no reCAPTCHA enterprise widget token response provided')
-      resolve(false)
-      return
-    }
-    request.post({
-      url: captchaEnterpriseUrl,
-      proxy: process.env.http_proxy,
-      qs: {
+async function verifyCAPTCHAEnterpriseVersion (token) {
+  if (!GOOGLE_RECAPTCHA_SECRET_KEY) {
+    throw new Error('reCAPTCHA secret key not set in environment')
+  }
+
+  if (!token) {
+    logger.warn('no reCAPTCHA enterprise widget token response provided')
+    return false
+  }
+
+  const response = await axios.post(
+    captchaEnterpriseUrl,
+    {
+      event: {
+        token: token,
+        siteKey: GOOGLE_RECAPTCHA_SITE_KEY
+      }
+    },
+    {
+      params: {
         key: GOOGLE_RECAPTCHA_SECRET_KEY
-      },
-      body: {
-        event: {
-          token: token,
-          siteKey: GOOGLE_RECAPTCHA_SITE_KEY
-        }
-      },
-      json: true
-    }, (error, response, body) => {
-      if (error) {
-        reject(error)
-        return
       }
-      if (response.statusCode === HTTP_SUCCESS_CODE) {
-        // https://cloud.google.com/recaptcha-enterprise/docs/interpret-assessment
-        if (!body.score || body.score < 0.9) {
-          logger.info('Failed reCAPTCHA response', {
-            tokenProperties: body.tokenProperties,
-            score: body.score,
-            reasons: body.reasons
-          })
-          resolve(false)
-          return
-        }
-        resolve(true)
-        return
-      }
-      reject(new Error(`Unknown reCAPTCHA response ${response.statusCode}`))
-    })
-  })
+    }
+  )
+
+  if (response.status === HTTP_SUCCESS_CODE) {
+    const body = response.data
+    // https://cloud.google.com/recaptcha-enterprise/docs/interpret-assessment
+    if (!body.score || body.score < 0.9) {
+      logger.info('Failed reCAPTCHA response', {
+        tokenProperties: body.tokenProperties,
+        score: body.score,
+        reasons: body.reasons
+      })
+      return false
+    }
+
+    return true
+  }
+
+  throw new Error(`Unknown reCAPTCHA response ${response.statusCode}`)
 }
 
-function verifyCAPTCHATokenBasicVersion (token) {
-  return new Promise((resolve, reject) => {
-    if (!GOOGLE_RECAPTCHA_SECRET_KEY) {
-      reject(new Error('reCAPTCHA secret key not set in environment'))
-      return
+async function verifyCAPTCHATokenBasicVersion (token) {
+  if (!GOOGLE_RECAPTCHA_SECRET_KEY) {
+    throw new Error('reCAPTCHA secret key not set in environment')
+  }
+  if (!token) {
+    throw new Error('no reCAPTCHA widget token response provided')
+  }
+
+  const form = new FormData() // eslint-disable-line
+  form.append('secret', GOOGLE_RECAPTCHA_SECRET_KEY)
+  form.append('response', token)
+
+  const response = await axios.post(
+    captchaBasicUrl,
+    form
+  )
+
+  if (response.status === HTTP_SUCCESS_CODE) {
+    const body = response.data
+    if (body.success !== true) {
+      logger.info('Failed reCAPTCHA response', {
+        errorCodes: body['error-codes']
+      })
     }
-    if (!token) {
-      reject(new Error('no reCAPTCHA widget token response provided'))
-      return
-    }
-    request.post({
-      url: captchaBasicUrl,
-      proxy: process.env.http_proxy,
-      formData: {
-        secret: GOOGLE_RECAPTCHA_SECRET_KEY,
-        response: token
-      },
-      json: true
-    }, (error, response, body) => {
-      if (error) {
-        reject(error)
-        return
-      }
-      if (response.statusCode === HTTP_SUCCESS_CODE) {
-        if (body.success !== true) {
-          logger.info('Failed reCAPTCHA response', {
-            errorCodes: body['error-codes']
-          })
-        }
-        resolve(body.success)
-        return
-      }
-      reject(new Error(`Unknown reCAPTCHA response ${response.statusCode}`))
-    })
-  })
+    return body.success
+  }
+
+  throw new Error(`Unknown reCAPTCHA response ${response.statusCode}`)
+}
+
+async function verifyCAPTCHAToken (token) {
+  if (process.env.GOOGLE_RECAPTCHA_USE_ENTERPRISE_VERSION === 'true') {
+    return await verifyCAPTCHAEnterpriseVersion(token)
+  } else {
+    return await verifyCAPTCHATokenBasicVersion(token)
+  }
 }
 
 module.exports = {
-  verifyCAPTCHAToken: GOOGLE_RECAPTCHA_USE_ENTERPRISE_VERSION === 'true' ? verifyCAPTCHAEnterpriseVersion : verifyCAPTCHATokenBasicVersion,
+  verifyCAPTCHAToken,
   formatEnterpriseUrl
 }
