cb811b641a5d6a131d2fa1898b68779681fdc583
Iqbal Ahmed <iqbal.ahmed@digital.cabinet-office.gov.uk>
2025-01-29 14:53:25 +0000

    PP-13016 - Remove unused JS and general cleanup
    - Removed client side validation JS files - old files that are no longer being used.
    - However, these files were making the JS bundle very large > 3MB.
      -  This is because of an issue with pay-js-commons JS bundling - which was bundling entire
         NPM modules.
    - Removed the 'validation-errors.njk' file - as this was only used by one of the old JS files.
    - Can also remove Nunjucksify - which was required to compile the earlier NJK file in JS.

--- app/browsered.js
+++ app/browsered.js
@@ -3,10 +3,4 @@
 // NPM dependencies
 const GOVUKFrontend = require('govuk-frontend')
 
-// Custom dependencies
-const fieldValidation = require('./browsered/field-validation')
-const inputConfim = require('./browsered/input-confirm')
-
-fieldValidation.enableFieldValidation()
-inputConfim()
 GOVUKFrontend.initAll()
