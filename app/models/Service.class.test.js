b34929af746150b47c140e533f6249cbadb01a4d
Iqbal Ahmed <iqbal.ahmed@digital.cabinet-office.gov.uk>
2025-07-24 12:44:55 +0100

    PP-14242 Footer bug fix - not showing address
    - Update service class to save merchant address in the right
      format when it comes back from the backend.
    - The address was not being shown in the footer because it
      was not being saving in the right format in the service class.
    - Update tests and stubs.
    - The country is currently being shown as 2 digit ISO code.  This
      will be updated in a future PR to show the full country name.

---
+++ app/models/Service.class.test.js
@@ -0,0 +1,44 @@
+'use strict'
+
+// NPM dependencies
+const expect = require('chai').expect
+
+// Local dependencies
+const Service = require('./Service.class')
+const serviceFixtures = require('../../test/fixtures/service.fixtures')
+
+describe('Service model from service raw data', () => {
+  it('should save merchant details correctly in the service model', () => {
+    const serviceModel = new Service(serviceFixtures.validServiceResponse({
+      organisationName: 'Give Me Your Money',
+      merchant_details: {
+        address_line1: 'Clive House',
+        address_line2: '10 Downing Street',
+        address_city: 'London',
+        address_postcode: 'AW1H 9UX',
+        address_country: 'GB'
+      }
+    }))
+
+    expect(serviceModel.organisationName).to.equal('Give Me Your Money')
+    expect(serviceModel.merchantDetails.name).to.equal('Give Me Your Money')
+    expect(serviceModel.merchantDetails.addressLine1).to.equal('Clive House')
+    expect(serviceModel.merchantDetails.addressLine2).to.equal('10 Downing Street')
+    expect(serviceModel.merchantDetails.city).to.equal('London')
+    expect(serviceModel.merchantDetails.postcode).to.equal('AW1H 9UX')
+    expect(serviceModel.merchantDetails.countryName).to.equal('GB')
+  })
+
+  it('should return merchant details as undefined when not in raw data', () => {
+    const data = {
+      external_id: '1234',
+      name: 'service name',
+      gateway_account_ids: [1],
+      custom_branding: { css_url: 'css url', image_url: 'image url' }
+    }
+
+    const serviceModel = new Service(data)
+
+    expect(serviceModel.merchantDetails).to.equal(undefined)
+  })
+})
