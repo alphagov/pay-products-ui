603f5cf9cd5198e24255b824dd585e0faa1f7b02
Iqbal Ahmed <iqbal.ahmed@digital.cabinet-office.gov.uk>
2025-10-23 09:39:27 +0100

    PP-14442 Removing inline JS script for translation strings
    - This script sets the translation strings in a JSON object.
    - This JSON object is never used.
    - Therefore removing it
    - Also means we don't have to deal with it in the CSP that we are
      setting up.

--- app/middleware/resolve-language.js
+++ app/middleware/resolve-language.js
@@ -6,7 +6,6 @@ const i18n = require('i18n')
 module.exports = function (req, res, next) {
   const language = req.product.language
   i18n.setLocale(req, language)
-  res.locals.translationStrings = JSON.stringify(i18n.getCatalog(language))
   res.locals.language = language
   next()
 }
